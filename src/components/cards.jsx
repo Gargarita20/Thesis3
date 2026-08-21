5334bcee6fa2265407f811e04222e2b9d197836d
Gargarita20 <pixeldust847@gmail.com>
2026-08-21 19:31:05 +0800

    feat: refactor Cards component to use props for company details and update home styles
    - adjust  card_wrapper to fill the rest of the page

--- src/components/cards.jsx
+++ src/components/cards.jsx
@@ -1,24 +1,23 @@
-import { Children } from 'react';
+import { Children, useState, useEffect } from 'react';
 import './cards.styles.css'
 import QRCode from 'react-qr-code';
 
 function Cards(prop){
     const icon = "H";
-    const company = "Honda Inc.";
+    const company = "AutoKID";
     const address = "Japan";
     const industry = "Automotive Tech";
     const qr_link = "sample";
     
-
     return (
         <div className="cards">
             <div className="bar"></div>
             <div className="content">
                 <div className="icon">{icon}</div>
                 <div className="description">
-                    <h1 className="company">{company}</h1>
-                    <h4 className="address">{address}</h4>
-                    <p className="industry">{industry}</p>
+                    <h1 className="company">{prop.company}</h1>
+                    <h4 className="address">{prop.address}</h4>
+                    <p className="industry">{prop.industry}</p>
                 </div>
             </div>
             <div className="qrcode">
